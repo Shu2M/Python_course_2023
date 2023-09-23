36baed4a31cd5a907adafe298c64c63b09e0f0c2
Shu2M <shulgame@bk.ru>
2023-09-23 10:13:29 +0300

    new gitignore

--- homework/hw3/case2/test.py
+++ homework/hw3/case2/test.py
@@ -16,7 +16,7 @@ teacher = Teacher()
 group_of_student = [Student() for _ in range(4)]
 
 for course in education_materials.materials:
-    num_students_on_course = random.randint(a=1, b=4)
+    num_students_on_course = random.randrange(4)
     teacher.teach(course, random.sample(group_of_student, k=num_students_on_course))
 
 for student in group_of_student:
