2afbcd1a48e76a07a09e58c14afcff2a740acf20
Shu2M <shulgame@bk.ru>
2023-09-24 19:36:02 +0300

    case9 ref

--- homework/hw4/case9/case9.py
+++ homework/hw4/case9/case9.py
@@ -1,4 +1,5 @@
 import collections
+from typing import Callable
 
 
 class AiInfoManager:
@@ -41,40 +42,53 @@ class AiInfoManager:
         top_usable = sorted(list(result), key=lambda x: len(x[1]))[0]
         return top_usable
 
-    def get_filtered_tools(self, cost: str = None, task: str = None, category: str = None):
+    def find_tools_by_cost(self, cost: str) -> set:
+        result = set()
+        for tool_name, cost_ in zip(
+                self.data['AI Tool Name'],
+                self.data['Free/Paid/Other'],
+        ):
+            if cost == cost_:
+                result |= {tool_name}
+        return result
+
+    def find_tools_by_task(self, task: str) -> set:
+        result = set()
+        for tool_name, usable in zip(
+                self.data['AI Tool Name'],
+                self.data['Usable For'],
+        ):
+            usable = [u.strip(' ') for u in usable.split(' / ') if u.strip()]
+            if task in usable:
+                result |= {tool_name}
+        return result
+
+    def find_tools_by_major_category(self, category: str) -> set:
+        result = set()
+        for tool_name, major_category in zip(
+                self.data['AI Tool Name'],
+                self.data['Major Category'],
+        ):
+            if category == major_category:
+                result |= {tool_name}
+        return result
+
+    def get_filtered_tools(
+            self,
+            cost: str = None,
+            task: str = None,
+            category: str = None,
+            key: Callable = None,
+    ) -> set:
         result_intersection = set(self.data['AI Tool Name'])
-
         if cost:
-            result = set()
-            for tool_name, cost_ in zip(
-                    self.data['AI Tool Name'],
-                    self.data['Free/Paid/Other'],
-            ):
-                if cost == cost_:
-                    result |= {tool_name}
-            result_intersection &= result
-
+            result_intersection &= self.find_tools_by_cost(cost)
         if task:
-            result = set()
-            for tool_name, usable in zip(
-                    self.data['AI Tool Name'],
-                    self.data['Usable For'],
-            ):
-                usable = [u.strip(' ') for u in usable.split(' / ') if u.strip()]
-                if task in usable:
-                    result |= {tool_name}
-            result_intersection &= result
-
+            result_intersection &= self.find_tools_by_task(task)
         if category:
-            result = set()
-            for tool_name, major_category in zip(
-                    self.data['AI Tool Name'],
-                    self.data['Major Category'],
-            ):
-                if category == major_category:
-                    result |= {tool_name}
-            result_intersection &= result
-
+            result_intersection &= self.find_tools_by_major_category(category)
+        if key:
+            result_intersection = set(key(result_intersection))
         return result_intersection
 
 
@@ -82,7 +96,8 @@ ai = AiInfoManager()
 ai.read_data()
 print(ai.free_or_paid_is_most_reviewed())
 print(ai.usable_with_most_free_tools())
+print(ai.get_filtered_tools(cost='Free', task='experiments', category='other', key=sorted))
 print(ai.get_filtered_tools(cost='Free', task='experiments', category='other'))
-
-
-
+print(ai.get_filtered_tools(cost='Free', task='experiments'))
+print(ai.get_filtered_tools(cost='Free'))
+print(ai.get_filtered_tools())
